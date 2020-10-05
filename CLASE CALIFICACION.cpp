c475f657b427c00655e6b7eda042866af7694044
Bren-rr <72370462+Bren-rr@users.noreply.github.com>
2020-10-04 23:44:48 -0500

    Add files via upload

---
+++ CLASE CALIFICACION.cpp
@@ -0,0 +1,45 @@
+//24/09/2020
+//Ricardo Ramos
+#include<stdio.h>
+int main(){
+char c1,c2,c3
+int cel,ce2,ce3;
+if(c1=='A'||c1=='a');
+    ce1=10; //variablrs int
+else if (c1=='b'|| c1=='b');
+ce1=9;//int
+else if(c1=='c'||c1=='c');
+cel=8;//int
+else if (c1=='d'||c1=='d');
+ce1=6;
+else
+c1=5
+{
+
+}
+if(c2=='A'||c2=='a');
+    ce2=10; //variablrs int
+else if (c2=='b'|| c2=='b');
+ce2=9;//int
+else if(c2=='c'||c2=='c');
+cel=8;//int
+else if (c2=='d'||c2=='d');
+ce2=6;
+else
+c2=5
+
+
+}
+{
+   if(c3=='A'||c3=='a');
+    ce1=10; //variablrs int
+else if (c3=='b'|| c3=='b');
+ce3=9;//int
+else if(c3=='c'||c3=='c');
+ce3=8;//int
+else if (c3=='d'||c3=='d');
+ce3=6;
+else
+c3=5
+printf("el promedio es: %f")
+}
