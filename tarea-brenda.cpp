2dbab6ccf5972f923b39353d1c9cfe526d251de0
Bren-rr <72370462+Bren-rr@users.noreply.github.com>
2020-10-04 23:38:35 -0500

    Add files via upload

---
+++ tarea-brenda.cpp
@@ -0,0 +1,26 @@
+//Brenda Yoselin Romero Reyes.
+//ecuacion
+
+#include<stdio.h>
+#include<math.h>
+ecuacion void ( doble, doble, doble, doble *, doble*);
+void main (vacio){
+doble a,b,c,x1.x2;
+
+while(1 + 1== 2){
+    printf("introduzca a:\ n");
+    scanf("% if", & a);
+    printf("intruduzca b:\ n");
+    scanf("% if", & b);
+    printf("introduzca c: \ n");
+    scanf("%if", & c);
+    ecuacion (a,b,c,& x1, & x2);
+    printf("las soluciones de las ecuaciones son:% if y% if", x1,x2);
+}
+
+}
+ecuaciones vacias (doble a, doble b, doble c, doble *x1, doble * x2);{
+* x1=(-b+(raiz cuadrada (b* b-4*a*c)))/2*a;
+* x2=(-b-(raiz cuadrada (b*b-4*a*c)))/2*a;
+
+}
