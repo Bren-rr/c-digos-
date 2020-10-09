6807d312ec9b4fa6eacdd2bf1554fa8512091274
Bren-rr <72370462+Bren-rr@users.noreply.github.com>
2020-10-08 19:38:03 -0500

    Add files via upload

---
+++ recuperacion-4c.cpp
@@ -0,0 +1,60 @@
+//Brenda Yoselin Romero Reyes
+//recuperación.
+
+
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("Dame el valor de n1:\n");
+    scanf("%d",&n1);
+    printf("dame el valor de n2:\n");
+    scanf("%d",&n2);
+
+    if(n1%2==0)
+    {
+        printf("n1 es par\n");
+    }
+
+    else
+    {
+        printf("n1 es impar\n");
+    }
+    if (n2%2==0)
+    {
+        printf("n1 es par\n");
+    }
+    else
+    {
+        printf("n1 es impar\n");
+    }
+    if (n2%2==0)
+    {
+        printf("n2 es par\n");
+    }
+    else
+    {
+        printf("n2 es impar\n")
+    }
+    if (n1>n2)
+    {
+        printf("n1 es el mayor");
+    }
+    else
+    {
+        printf("n2 es el mayor");
+        return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
