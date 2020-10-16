0b4c7cc3b619dafe5e22c8f060234e1cec3a56fe
Bren-rr <72370462+Bren-rr@users.noreply.github.com>
2020-10-15 20:53:48 -0500

    Add files via upload

---
+++ 05-contador2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int mail()
+{
+     int N,i;
+     car opc='s';
+     while(opc=='s'){
+        i=0;
+        printf("escribe el valor de N:");
+        scanf("%d",&N);
+        while(i<=N)
+     }
+     printf("%d",N*N-2*i);
+     i=i+1;
+     {
+         printf("\nquieres ver otros valores s\n:");
+         scanf("%s",&opc);
+     }
+     printf("\nprograma finalizado");
+     return 0;
+}
